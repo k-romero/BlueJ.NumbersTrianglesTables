5cbbb68fcb4efc2fc0c6d6529bb5a647d9c1c474
k-romero <imkevinromero@gmail.com>
2020-02-06 09:01:17 -0500

    completed the triangles portion of the lab using nested for loops

--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -3,19 +3,51 @@
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String myRow = "";
+        String mySymbol = "*";   
+        for (int i = 0; i < numberOfStars; i++){
+            myRow += mySymbol;
+        }
+        return myRow;
     }
     
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String myRow = "";
+        String mySymbol = "*";
+        String myBreak = "\n";
+        for (int i = 1; i <= numberOfRows; i++){
+            for (int j = 0; j < i; j++){
+                myRow += mySymbol;
+            }
+            myRow += myBreak;
+        }
+       return myRow;
     }
 
 
     public static String getSmallTriangle() {
-        return null;
+        String myRow = "";
+        String mySymbol = "*";
+        String myBreak = "\n";
+        for (int i = 1; i <= 4; i++){
+            for (int j = 0; j < i; j++){
+                myRow += mySymbol;
+            }
+            myRow += myBreak;
+        }
+       return myRow;
     }
 
     public static String getLargeTriangle() {
-        return null;
+        String myRow = "";
+        String mySymbol = "*";
+        String myBreak = "\n";
+        for (int i = 1; i <= 9; i++){
+            for (int j = 0; j < i; j++){
+                myRow += mySymbol;
+            }
+            myRow += myBreak;
+        }
+       return myRow;
     }
 }
