077ea3c348ee9b8c81c24be782a56964f0fc682b
k-romero <imkevinromero@gmail.com>
2020-02-06 15:44:04 -0500

    completed tables portion of the lab using nested for loops and conditionals

--- TableUtilities.java
+++ TableUtilities.java
@@ -2,14 +2,31 @@
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String myRow = "";
+        String singleSpace = " ";
+        String doubleSpace = "  ";
+        String mySymbol = " |";
+        String myBreak = "\n";
+        for (int i = 1; i <= tableSize; i++){
+            for (int j = 1; j <= tableSize; j++){
+                if (j*i < 10){
+                    myRow += doubleSpace;
+                } else if (j*i < 100) {
+                    myRow += singleSpace;
+                }
+                myRow += j*i;
+                myRow += mySymbol;
+            }
+            myRow += myBreak;
+        }
+       return myRow;
     }
 }
